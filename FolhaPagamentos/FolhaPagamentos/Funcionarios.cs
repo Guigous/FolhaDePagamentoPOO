20388ffa5a602443ebae92ab2f3c0467c6ac9f6a
Guigous <gui_1810@hotmail.com>
2021-12-01 20:54:24 -0300

    Codigo comentado

---
+++ FolhaPagamentos/FolhaPagamentos/Funcionarios.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FolhaPagamentos
+{
+    public class Funcionarios
+    {//encapsulamento
+        public string Nome { get; set; }
+        public double HorasTrabalhadas { get; set; }
+        public double ValorPorHora { get; set; }
+        //construtor
+        public Funcionarios(string nome, double horasTrabalhadas, double valorPorHora)
+        {
+            Nome = nome;
+            HorasTrabalhadas = horasTrabalhadas;
+            ValorPorHora = valorPorHora;
+        }
+
+        //metodo de calculo do salario
+        public virtual double CalcSalario()
+        {
+            double salario = HorasTrabalhadas * ValorPorHora;
+            return salario;
+        }
+
+        
+    }
+
+
+
+
+    public class Tercerizado : Funcionarios
+    {
+        //encapsulamento
+        public double Despesa { get; set; }
+
+       
+
+        //construtor
+        public Tercerizado(string nome, double horasTrabalhadas, double valorPorHora, double despesa)
+            : base(nome, horasTrabalhadas, valorPorHora)
+        {
+            Despesa = despesa;
+        }
+
+        
+        //metodo do calculo do tercerizado
+        public override double CalcSalario()
+        {
+            
+            double salarioterc = (HorasTrabalhadas*ValorPorHora)+Despesa*1.1;
+            return salarioterc;
+
+        }
+    }
+
+  
+            
+
+       
+    
+}
---
+++ FolhaPagamentos/FolhaPagamentos/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FolhaPagamentos
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {//instaciamento da lista
+            List<Funcionarios> funcionarios = new List<Funcionarios>();
+            Console.WriteLine("FOLHA DE PAGAMENTO------------------ ");
+            Console.WriteLine();
+            Console.WriteLine("Numero de Funcionarios a serem Listados: ");
+            Console.WriteLine();
+
+            //numero indicado pelo usuario
+            int n = int.Parse(Console.ReadLine());
+            //loop para input de dados
+            for( int i=0; i<n; i++ )
+            {
+                Console.WriteLine("Digite (s) para Simples e (t) Para Tercerizado");
+                char resposta =char.Parse(Console.ReadLine());
+                //condicional
+                if (resposta == 's')
+                {
+                    Console.WriteLine("Nome Do Funcionario: ");
+                    string nome = Console.ReadLine();
+
+                    Console.WriteLine("Horas Trabalhadas: ");
+                    double horasTrabalhadas = double.Parse(Console.ReadLine());
+
+                    Console.WriteLine("Valor da Hora: ");
+                    double valorPorHora = double.Parse(Console.ReadLine());
+                    
+                    funcionarios.Add(new Funcionarios (nome, horasTrabalhadas, valorPorHora ));
+
+                    double salarios = funcionarios[i].CalcSalario();
+
+
+                }
+                //condicional
+                if(resposta == 't')
+                {
+                    
+
+                    Console.WriteLine("Nome Do Funcionario: ");
+                    string nome = Console.ReadLine();
+
+                    Console.WriteLine("Horas Trabalhadas: ");
+                    double horasTrabalhadas = double.Parse(Console.ReadLine());
+
+                    Console.WriteLine("Valor da Hora: ");
+                    double valorPorHora = double.Parse(Console.ReadLine());
+
+                    Console.WriteLine("Despesas: ");
+                    double despesa = double.Parse(Console.ReadLine());
+
+
+                    funcionarios.Add(new Tercerizado (nome, horasTrabalhadas, valorPorHora, despesa ));
+
+                    
+                }
+            }
+            //output de dados
+            foreach(Funcionarios funcionarios1 in funcionarios)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Nome: " + funcionarios1.Nome);
+                Console.WriteLine("Horas Trabalhadas: " + funcionarios1.HorasTrabalhadas); 
+                Console.WriteLine("Valor da Hora: " + funcionarios1.ValorPorHora);
+                Console.WriteLine("Salario: " + funcionarios1.CalcSalario());
+                Console.WriteLine();
+            }
+        }
+    }
+}
